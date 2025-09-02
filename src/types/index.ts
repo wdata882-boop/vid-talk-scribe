953afeca120a6342da4f052977a71acb51c9019f
wdata882-boop <wdata882@gmail.com>
2025-09-02 18:02:27 +0630

    Delete src directory

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface VideoFile {
-  name: string;
-  size: number;
-  type: string;
-  file: File;
-}
-
-export type ProcessingState = "idle" | "transcribing" | "translating" | "generating" | "completed";
